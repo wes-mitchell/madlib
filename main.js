2fc0527badead8ae6d8de986b3c012405dd8f199
wes-mitchell <wmdrums@gmail.com>
2022-03-29 15:22:14 -0500

    completes project utilizing session storage to create a madlib

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 console.log("You look marvelous!");
 
-//story
-// It had been a hard, {adjective} day on the {silly word} trail. The cowboys drove a herd of {plural noun} across the dry plains, kicking up {noun} along the way as they looked for somewhere to bed down.
-
 const mainElement = document.querySelector("main");
 
 mainElement.addEventListener("click", event => {
 	if(event.target.id.startsWith("tellStory")){
 		const madlib = {
-			adjective:"",
-			sillyword:"",
-			pluralnoun:"",
-			noun:""
+			adjective: document.querySelector("input[name='adjective']").value,
+			sillyword: document.querySelector("input[name='sillyword']").value,
+			pluralnoun: document.querySelector("input[name='pluralNoun']").value,
+			noun: document.querySelector("input[name='noun']").value
 		}
 		//set/save to sessionStorage
-		
+		setDataToStorage(madlib)
 		//invoke renderStory
+		renderStory()
 	}
 })
 
+// object to come back
+
 const getDataFromStorage = (dataKey) => {
+	return JSON.parse(sessionStorage.getItem(dataKey))
 	//use JSON.parse()
 }
 
+// string to go to storage
+
 const setDataToStorage = (dataObj) => {
+	sessionStorage.setItem("madlib", JSON.stringify(dataObj)) // 'dataObj': '{}'
 	//use JSON.stringify()
 }
 
@@ -31,20 +35,38 @@ const clearStorage = (dataKey) => {
 	sessionStorage.removeItem(dataKey);
 }
 
-const renderInputs = () => {
-	clearStorage('madlib')
-	// show inputs fields
 
-	//show 'Tell Story' button
+const renderInputs = () => {
+    clearStorage('madlib')
+    let mainEl = document.querySelector("main")
+    mainEl.innerHTML = `
+        <label for="adjective">Adjective:</label>
+        <input name="adjective" id="adjective-input"><br>
+        <label for="sillyword">sillyWord:</label>
+        <input name="sillyword" id="sillyWord-input"><br>
+        <label for="pluralNoun">Plural Noun:</label>
+        <input name="pluralNoun" id="pluralNoun-input"><br>
+        <label for="noun">Noun:</label>
+        <input name="noun" id="noun-input"><br>
+        <button type="submit" id="tellStory">Tell Story</button>
+    `
 }
+
 
 const renderStory = () => {
 	//get from sessionStorage
-
-	//show the story
-
+	const lib =	getDataFromStorage('madlib')
+	mainElement.innerHTML = `It had been a hard, ${lib.adjective} day on the ${lib.sillyword} trail. The cowboys drove a herd of ${lib.pluralnoun} across the dry plains, kicking up ${lib.noun} along the way as they looked for somewhere to bed down.`
 	//show startOver button
+	mainElement.innerHTML += `<br>
+	<button type="submit" id="start-over">Start the F Over</button>`
 	//startOver will invoke renderInputs()
 }
 
 renderInputs();
+
+mainElement.addEventListener("click", event => {
+	if (event.target.id === "start-over") {
+		renderInputs()
+	}
+})
